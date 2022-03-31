27d239329abfad7bfade3eb0ad38d6d178418f0b
Dimitrios Ieronymakis <dimitris.ieronymakis@gmail.com>
2022-03-31 18:46:27 +0200

    implemented backwards step + gd

--- Model.py
+++ Model.py
@@ -12,11 +12,27 @@ class TwoLayerPerceptron:
         self.activation = activation
         self.error_function = error_function
 
-    def forward_pass(self, input):
+    def forward_pass(self, input, return_activations=False):
         hidden_layer_out = np.dot(input, self.hidden_weights) + self.hidden_bias
         hidden_layer_out = self.activation(hidden_layer_out)
 
         output_layer_out = np.dot(hidden_layer_out, self.output_weights) + self.hidden_bias
         output_layer_out = self.activation(output_layer_out)
 
+        if return_activations:
+            return hidden_layer_out, output_layer_out
         return output_layer_out
+
+    def gradient_descent(self, X, y_true):
+        # Backpropagation step
+        hidden_layer_out, output_layer_out = self.forward_pass(X,return_activations=True)
+        error = self.error_function(y_true, output_layer_out)
+        d_predicted_output = error * self.activation.derivative(output_layer_out)
+
+        error_hidden_layer = np.dot(d_predicted_output,self.output_weights)
+        d_hidden_layer = error_hidden_layer * self.activation.derivative(hidden_layer_out)
+        return d_predicted_output, d_hidden_layer
+
+    def train(self, X, y, lr, epochs):
+        for i in epochs:
+            pass
--- test.py
+++ test.py
@@ -1,4 +1,14 @@
 import numpy as np
+from Error_functions import *
+from Activations import *
+from Model import *
 
-x = np.array([[0,0],[0,1],[1,0],[1,1]])
+X = np.array([[0,0],[0,1],[1,0],[1,1]])
 y = np.array([[0],[1],[1],[0]])
+activation_function = Sigmoid()
+error_function = mse
+
+model = TwoLayerPerceptron(2,2,1,activation_function,error_function)
+pred_y = model.forward_pass(X)
+
+print(model.gradient_descent(X, y))
