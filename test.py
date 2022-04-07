ea460715fbc3eef264c07d6f0db53ac36ebd6cac
Dimitrios Ieronymakis <dimitris.ieronymakis@gmail.com>
2022-04-08 01:28:34 +0200

    refactored test

--- test.py
+++ test.py
@@ -3,27 +3,21 @@ import matplotlib.pyplot as plt
 from Error_functions import mse
 from Activations import *
 from Model import TwoLayerPerceptron
-
-#np.random.seed(0)
-
+# Set seed for reproducibility
+np.random.seed(0)
+# Data
 X = np.array([[0,0],[0,1],[1,0],[1,1]])
 y = np.array([[0],[1],[1],[0]])
+
 activation_function = Sigmoid()
 error_function = mse
-
 model = TwoLayerPerceptron(2,2,1,activation_function,error_function)
-#pred_y = model.forward_pass(X)
 
-#print(model.gradient_descent(X, y,0.01))
-
-"""print("predictions:",model.forward_pass(X))
-print("hidden weights:",model.hidden_weights)
-print("output weights:",model.output_weights)"""
+# Train the model
 loss = model.train(X, y, 0.1, 10000)
-print("predictions:",model.forward_pass(X))
-#print("hidden weights:",model.hidden_weights)
-#print("output weights:",model.output_weights)
 
+# Show results
+print("predictions:",model.forward_pass(X))
 plt.plot(loss)
 plt.show()
 
